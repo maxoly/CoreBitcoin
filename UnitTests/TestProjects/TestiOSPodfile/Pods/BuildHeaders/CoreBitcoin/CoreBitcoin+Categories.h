3f6177b16cbcac200e825145a8c1dabf5ddb0863
Massimo Oliviero <massimo.oliviero@gmail.com>
2017-02-10 16:53:23 +0100

    Changed OpenSSL-Universal dependency version

--- UnitTests/TestProjects/TestiOSPodfile/Pods/BuildHeaders/CoreBitcoin/CoreBitcoin+Categories.h
+++ UnitTests/TestProjects/TestiOSPodfile/Pods/BuildHeaders/CoreBitcoin/CoreBitcoin+Categories.h
@@ -1,1 +1,5 @@
-../../CoreBitcoin/CoreBitcoin/CoreBitcoin+Categories.h
+// CoreBitcoin by Oleg Andreev <oleganza@gmail.com>, WTFPL.
+
+#import <CoreBitcoin/CoreBitcoin.h>
+#import <CoreBitcoin/NS+BTCBase58.h>
+#import <CoreBitcoin/NSData+BTCData.h>
